7723d1ed86bb4d13d6dfa26157e3ab9b3f4f438b
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-08 00:50:30 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -13,7 +13,8 @@ print("without using append() method")
 l1 = eval(input("Enter list :"))
 print("list =",l1)
 
-'''using append() method
+''' Output :-
+using append() method
 Enter length of a list: 4
 Enter elements of list:
 454
@@ -24,3 +25,68 @@ list = [454, 22, 778, 55]
 without using append() method
 Enter list :[10,'a',789,"abc",43]
 list = [10, 'a', 789, 'abc', 43]'''
+
+# 2.Add element at first, last and specific location in list 
+
+print("add element at first location")
+
+list = eval(input("list ="))
+list.insert(0,"Mango")
+print(list)
+
+print("add element at specific location")
+
+list1 = eval(input("list1 ="))
+list1.insert(2,"Mango")
+print(list1)
+
+print("add element at last location") 
+
+list2 = eval(input("list2 ="))
+list2.append("Mango")
+print(list2)
+'''
+Output :-
+
+add element at first location
+list = ["Orange",'a',34,"$",23.4]
+['Mango', 'Orange', 'a', 34, '$', 23.4]
+
+add element at specific location
+list1 = ["Orange",'a',34,"$",23.4]
+['Orange', 'a', 'Mango', 34, '$', 23.4]
+
+add element at last location
+list2 =  ["Orange",'a',34,"$",23.4] 
+['Orange', 'a', 34, '$', 23.4, 'Mango']'''
+
+# 3.Remove element from first position ,last position and specific position from list.
+
+print("Remove element form first position -->")
+l1 = eval(input("Enter list ="))
+l1.remove(l1[0])
+print(l1)
+
+print("Remove element form specific position-->")
+l2 = eval(input("Enter list ="))
+l2.remove(l2[2])
+print(l2)
+
+print("Remove element form last position-->")
+l3 = eval(input("Enter list ="))
+l3.remove(l3[-1])
+print(l3)
+
+'''Output :-
+ 
+ Remove element form first position -->
+Enter list = [11,34,"Kavita",54.9,'b',20] 
+[34, 'Kavita', 54.9, 'b', 20]
+Remove element form specific position-->
+Enter list = [11,34,"Kavita",54.9,'b',20] 
+[11, 34, 54.9, 'b', 20]
+Remove element form last position-->
+Enter list =  [11,34,"Kavita",54.9,'b',20]
+[11, 34, 'Kavita', 54.9, 'b']'''
+
+ 
