5cb09aa73b9eb12a72504a1735fdb37c64a898df
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-16 06:17:42 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -1,1 +1,12 @@
 
+# 1.Write a Python program to get the frequency of the elements in a list
+
+l1 = eval(input("Enter list :"))
+count = {}
+for x in l1:
+    if x in count:
+        count[x] += 1
+    else:
+        count[x] = 1
+print("frequency of a element in a list =\n",list(count.items()))
+
