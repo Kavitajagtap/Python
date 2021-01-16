1a4136bdf6cd3da7c401a022e1596f7086d3a724
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-16 06:18:05 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -10,3 +10,14 @@ for x in l1:
         count[x] = 1
 print("frequency of a element in a list =\n",list(count.items()))
 
+# 2.write a  program to count the number of elements in a list within a specified range.
+
+list = eval(input("Enter list:"))
+min = int(input("minimum:"))
+max = int(input("maximum:"))
+c = 0
+for x in list:
+ if min <= x <= max:
+  c += 1
+print("number of element within ",min,"to",max,"=",c)
+
