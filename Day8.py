9651c0a7afeaba9b2df0647ebb8e92d33d775d06
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-17 11:34:44 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -6,3 +6,16 @@ print("Accessing Elements from dictionary -->")
 for key in dict:
     print(key,dict[key])
 
+# 2.Write a Program to sort (ascending and descending) a dictionary by value.
+
+dict = {}
+n = int(input("Enter elements: "))
+for i in range(n):
+    k = (input("key: "))
+    v = int(input("value: "))
+    dict[k] = v
+print("dictionary =",dict)
+print("sorted dictionary in ascending order =",sorted(dict.items(), key=lambda x: x[1]))
+print("sorted dictionary in descending order =",sorted(dict.items(), key=lambda x: x[1],reverse = True))
+
+
