45cd369855be4245701586b6c574cb9995c7cb2d
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-17 11:41:10 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -5,6 +5,16 @@ dict = eval(input("Enter dictionary = "))
 print("Accessing Elements from dictionary -->")
 for key in dict:
     print(key,dict[key])
+'''
+Output :
+
+Enter dictionary = {'Apple':2017,'Microsoft':1985,'Facebook':2012,'Amazon':1997}
+Accessing Elements from dictionary -->
+Apple 2017
+Microsoft 1985
+Facebook 2012
+Amazon 1997
+'''
 
 # 2.Write a Program to sort (ascending and descending) a dictionary by value.
 
@@ -17,6 +27,22 @@ for i in range(n):
 print("dictionary =",dict)
 print("sorted dictionary in ascending order =",sorted(dict.items(), key=lambda x: x[1]))
 print("sorted dictionary in descending order =",sorted(dict.items(), key=lambda x: x[1],reverse = True))
+'''
+Output:
+
+Enter elements: 4
+key: a
+value: 4
+key: b
+value: 1
+key: c
+value: 6
+key: d
+value: 3
+dictionary = {'a': 4, 'b': 1, 'c': 6, 'd': 3}
+sorted dictionary in ascending order = [('b', 1), ('d', 3), ('a', 4), ('c', 6)]
+sorted dictionary in descending order = [('c', 6), ('a', 4), ('d', 3), ('b', 1)]
+'''
 
 
 # 3.Write a Program to add a key to a dictionary.
