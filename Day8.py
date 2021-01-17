5f82af3147f21e4b3e1859a4da9be7b889c2ed66
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-17 11:23:34 +0530

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,8 @@
+
+# 1.Write a program to create dictionary and access all elements with keys and values
+
+dict = eval(input("Enter dictionary = "))
+print("Accessing Elements from dictionary -->")
+for key in dict:
+    print(key,dict[key])
+
