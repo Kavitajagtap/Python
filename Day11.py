b7374477ab18c73911c9c9e9f3752f9660d1ba91
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-18 06:03:34 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -1,1 +1,10 @@
+# 1.Write a  program to print all unique values in a dictionary.
 
+dict = {}
+n = int(input("Enter elements: "))
+for i in range(n):
+    key = int(input("key: "))
+    value = int(input("value: "))
+    dict[key] = value
+print("Dictionary =",dict)
+print("unique values in a dictionary =",set(dict.values()))
