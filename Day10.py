81400f243e0438dc48871f08006e87225d9d41dd
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-18 06:01:06 +0530

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -4,8 +4,8 @@
 dict = {}
 n= int(input("Enter elements ="))
 for i in range(n):
-    key = int(input("key: "))
-    value = int(input("value: "))
+   key = int(input("key: "))
+   value = int(input("value: "))
     dict[key] = value
 print("Dictionary =",dict)
 s = int(input("Enter any key to remove: "))
