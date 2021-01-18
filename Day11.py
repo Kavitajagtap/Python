6caefdb897f7d3524fbc2ae2daa7dcaa5b20d27b
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-18 06:04:29 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -8,3 +8,19 @@ for i in range(n):
     dict[key] = value
 print("Dictionary =",dict)
 print("unique values in a dictionary =",set(dict.values()))
+
+# 2.Write a  program to create and display all combinations of letters, selecting each letter from a different key in a dictionary.
+dict = {}
+n = int(input("Enter elements: "))
+for i in range(n):
+    key = int(input("key: "))
+    value = eval(input("value: "))
+    dict[key] = value
+print("Dictionary =",dict)
+l1 = list(dict.values())
+for i in l1[0]:
+    for j in range(1, len(l1)):
+        for a in l1[j]:
+            dict1 = i + a
+            print(dict1)
+
