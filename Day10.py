ee6c64b1cb6dddf95116d860d78b9dec49f06d3d
Kavitajagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-18 05:43:52 +0530

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -12,4 +12,16 @@ if s in dict:
   del dict[s]
 print("Updated dictionary =",dict)
 
-# 
+# 2. Write a  program to multiply all the items in a dictionary.
+
+dict = {}
+n= int(input("Enter elements ="))
+for i in range(n):
+    key = (input("key: "))
+    value = int(input("value: "))
+    dict[key] = value
+print("Dictionary =",dict)
+d = 1
+for i in dict:
+    d = d * dict[i]
+print("Multiplication of all elements in dictionary =",d)
