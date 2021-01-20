7a3d720275c30cce3ad8b1730a3f31a6e53e52ee
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-20 06:20:51 +0530

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -3,5 +3,22 @@
 Sample data: [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
 Expected Output: Counter({'item1': 1150, 'item2': 300})
 """
+d1 = eval(input("Enter dict1:"))
+d2 = eval(input("Enter dict1:"))
+d3 = eval(input("Enter dict1:"))
+l1 = [d1,d2,d3]
+print("list of dictionaries =",l1)
+
+list = []
+for d in l1:
+    list.append(tuple(d.values()))
+dict = {}
+for i in list:
+    if i[0] in dict:
+        dict[i[0]] += i[1]
+    else:
+        dict[i[0]] = i[1]
+print("new dictionary =",dict)
+
 
 
