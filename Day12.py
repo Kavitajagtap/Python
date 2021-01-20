a2f0e5443268b7a3d8522bc802e20301a87c64b9
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-20 06:20:10 +0530

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -1,1 +1,7 @@
+"""
+1.Write a Python program to combine values in python list of dictionaries. Go to the editor
+Sample data: [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
+Expected Output: Counter({'item1': 1150, 'item2': 300})
+"""
+
 
