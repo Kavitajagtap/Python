7ea572eba83e0efac80b54ce2ad42ba1322244c8
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-20 06:22:35 +0530

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -25,7 +25,7 @@ print("new dictionary =",dict)
 Note: Track the count of the letters from the string.
 Sample string : 'w3resource'
 Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
-'''
+"""
 s1 = input("Enter a string:")
 print("String is:",s1)
 count = {}
