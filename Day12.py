1d2192260728c7e1f7c5742ea91d3d0a4f19aaa2
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-20 06:21:33 +0530

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -20,5 +20,11 @@ for i in list:
         dict[i[0]] = i[1]
 print("new dictionary =",dict)
 
+"""
+2.Write a Python program to create a dictionary from a string. Go to the editor.
+Note: Track the count of the letters from the string.
+Sample string : 'w3resource'
+Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
+'''
 
 
