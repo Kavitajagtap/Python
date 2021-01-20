9812e57d35eb0f20552825313c1244a5f402b0bb
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-20 06:22:05 +0530

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -26,5 +26,14 @@ Note: Track the count of the letters from the string.
 Sample string : 'w3resource'
 Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
 '''
+s1 = input("Enter a string:")
+print("String is:",s1)
+count = {}
+for x in s1:
+    if x in count.keys():
+        count[x] += 1
+    else:
+        count[x] = 1
+print("count of the letters =",count)
 
 
