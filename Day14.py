b905feb8abac47e56735f805fe9fa394fa8af174
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-22 06:00:19 +0530

    Update Day14.py

--- Day14.py
+++ Day14.py
@@ -1,6 +1,9 @@
+"""
+ 1.Write a program to sort a list alphabetically in a dictionary
+Enter dictionary = {1: ['B', 'C', 'A'], 2: ['D', 'B', 'A'], 3: ['V', 'A', 'K']}
+Sorted list of dictionary = {1: ['A', 'B', 'C'], 2: ['A', 'B', 'D'], 3: ['A', 'K', 'V']}
 
-# 1.Write a program to sort a list alphabetically in a dictionary.
-
+"""
 dict = {}
 n = int(input("Enter elements: "))
 for i in range(n):
@@ -14,7 +17,8 @@ print("Sorted list of dictionary =",new_dict)
 
 ------------------------------------------------------------------------------------------------------------
 
-# 2.Write a program to remove spaces from dictionary keys.
+# 2.Write a program to remove spaces from dictionary keys
+
 dict = {}
 n = int(input("Enter elements: "))
 
