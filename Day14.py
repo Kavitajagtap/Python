0a08796f2ea8c82de583ae52889aafea667bebd9
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-22 05:48:15 +0530

    Update Day14.py

--- Day14.py
+++ Day14.py
@@ -16,11 +16,13 @@ print("Sorted list of dictionary =",new_dict)
 # 2.Write a program to remove spaces from dictionary keys.
 dict = {}
 n = int(input("Enter elements: "))
+
 for i in range(n):
     k = (input("key: "))
     v = int(input("value: "))
     dict[k] = v
 print("Dictionary =",dict)
+
 for key in dict:
     k1 = key.replace(' ', '')
     dict[k1] = dict.pop(key)
