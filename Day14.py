ba79191f5878676fb0c0b170cc55439ec90e3700
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-22 05:40:37 +0530

    Update Day14.py

--- Day14.py
+++ Day14.py
@@ -1,3 +1,14 @@
 # 1.Write a program to sort a list alphabetically in a dictionary.
 
+dict = {}
+n = int(input("Enter elements: "))
+for i in range(n):
+    k = int(input("key: "))
+    v = eval(input("value: "))
+    dict[k] = v
+print("Dictionary =",dict)
+new_dict = {k: sorted(v) for k, v in dict.items()}
+
+print("Sorted list of dictionary =",new_dict)
+
 
