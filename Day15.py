de1f83fa72fb5922b784101dd1ad9b822fa050bb
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-22 06:06:12 +0530

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -1,2 +1,13 @@
 
 # 1. Write a program to print a dictionary line by line.
+
+dict = {}
+n = int(input("Enter elements: "))
+for i in range(n):
+    k = (input("key: "))
+    v = int(input("value: "))
+    dict[k] = v
+print("dictionary = ",dict,"\nDictionary line by line: ")
+for k,v in dict.items():
+    print(k)
+    print(v)
