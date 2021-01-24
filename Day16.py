7951c7adcd87aa1bbfba1fc8c2ef56059bc2c4a3
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-24 10:52:32 +0530

    Update Day16.py

--- Day16.py
+++ Day16.py
@@ -3,4 +3,13 @@
 Sample data : {'Math':81, 'Physics':83, 'Chemistry':87}
 Expected data: [('Chemistry', 87), ('Physics', 83), ('Math', 81)]
 """
+dict = {}
+n = int(input("Enter elements: "))
+for i in range(n):
+    k = (input("key: "))
+    v = int(input("value: "))
+    dict[k] = v
+print("dictionary = ",dict)
+print("Dictionary sorted by value = ")
+print(sorted(dict.items(), key=lambda x: x[1], reverse=True))
 
