0042011721fc4adcc493fb2cce33162c814b3013
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-24 11:08:27 +0530

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -34,10 +34,11 @@ print("Original dictionary =",dict)
 dict = {k:v for (k, v) in dict.items() if v is not None}
 print("Dictionary after dropping empty items:",dict)
 
+------------------------------------------------------------------------------------------------------------
 
 """
-3.Write a program to create a dictionary of keys x, y, and z where each key has as value a list from 11-20, 21-30, and 31-40 respectively. Access the fifth value of each key 
-from the dictionary.
+3.Write a program to create a dictionary of keys x, y, and z where each key has as value a list from 11-20, 21-30, and 31-40 respectively.
+Access the fifth value of each key from the dictionary.
 
 """
 d = {}
