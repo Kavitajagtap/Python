686e419380dd0621cd23376ab07ae1ba5845755a
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-24 11:03:50 +0530

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -34,7 +34,8 @@ print("Dictionary after dropping empty items:",dict)
 
 """
 3.Write a program to create a dictionary of keys x, y, and z where each key has as value a list from 11-20, 21-30, and 31-40 respectively. Access the fifth value of each key 
-from the dictionary
+from the dictionary.
+
 """
 d = {}
 n = int(input("Enter elements of dictionary: "))
