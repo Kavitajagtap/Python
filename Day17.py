4f511e2b9be85be01c96762fcca54b6e3e4794d2
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-24 10:59:12 +0530

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -1,1 +1,25 @@
+"""
+1. Write a program to match key values in two dictionaries.
+Sample dictionary: {'key1': 1, 'key2': 3, 'key3': 2}, {'key1': 1, 'key2': 2}
+Expected output: key1: 1 is present in both x and y
+"""
+def dict1():
+ d = {}
+ n = int(input("Enter elements of dictionary: "))
+ for i in range(n):
+   key = (input("key: "))
+   value = int(input("value: "))
+   d[key] = value
+ return d
+d1 = dict1()
+d2 = dict1()
+print("dict1 =",d1,"\ndict2 =",d2)
 
+for (key, value) in (d1.items() & d2.items()):
+    print('{}:'.format(key), value,'is present in both lists')
+"""
+2. Write program to drop empty items from a given dictionary. 
+Original dictionary :
+{'c1':'Red','c2':'Pink','c3':None}
+Output :
+{'c1':'Red','c2':'Pink'}
