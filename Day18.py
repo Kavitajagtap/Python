83d167b9931cfaf6ec157171f66daa3964c4522b
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-27 06:37:06 +0530

    Update Day18.py

--- Day18.py
+++ Day18.py
@@ -11,6 +11,9 @@ for x in s1:
     else:
         count[x] = 1
 print("count of the characters =",count)
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
 """
 2.Write a  program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself. 
 Sample String : 'restart'
