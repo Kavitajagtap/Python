2d1ad840f922072b2e14a393e30352b19cd8e8e8
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-27 06:34:14 +0530

    Update Day18.py

--- Day18.py
+++ Day18.py
@@ -1,1 +1,13 @@
-
+"""
+1. Write a  program to count the number of characters (character frequency) in a string
+Sample String : ‘google.com'
+Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+"""
+s1 = input("Enter any string = ")
+count = {}
+for x in s1:
+    if x in count.keys():
+        count[x] += 1
+    else:
+        count[x] = 1
+print("count of the characters =",count)
