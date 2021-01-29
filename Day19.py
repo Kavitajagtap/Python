483ad1897df0de68bd0335c2df1218acdadaec8b
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2021-01-29 23:06:16 +0530

    Update Day19.py

--- Day19.py
+++ Day19.py
@@ -1,1 +1,18 @@
+## 1.Write a Python function that takes a list of words and returns the longest word and the length of the longest one.
+ def long_word(list):
+    length = []
+    for i in list:
+        length.append((len(i), i))
+    length.sort()
+    return length[-1][0], length[-1][1]
 
+list = []
+n = int(input("length of a list = "))
+print("elements of list:")
+for i in range(n):
+    list.append(input())
+print("list =",list)
+
+a = long_word(list)
+print("Longest word =",a[1])
+print("Length of the longest word =",a[0])
